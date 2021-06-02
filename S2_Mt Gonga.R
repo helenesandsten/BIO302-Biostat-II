dbff5e15d344313b3da754a6f553138b6697e9a4
helenesandsten <sandstenhelene@gmail.com>
2021-06-02 16:37:18 +0200

    aud helped me fix github

--- S2_Mt Gonga.R
+++ S2_Mt Gonga.R
@@ -5,6 +5,32 @@
 library(tidyverse)
 library(readxl)
 
+#### NEW AND IMPROVED MT GONGA STUFF ####
+# Importing data and making dataframe 'mtgonga'
+mtgonga <- "biomass2015.xls" 
+
+# Making object with data from all sheets of 'mtgonga' 
+mtg_data <- excel_sheets(mtgonga) %>% 
+  map_dfr(~ read_excel(mtgonga, sheet = .x))
+
+# Simple boxplot 
+ggplot(mtg_data, aes(x = site, y = production), group = site) +
+  geom_boxplot() +
+  ggtitle("Mount Gonga - Biomass") + 
+  theme_bw()
+  
+
+
+# 
+?map_dfr
+1:4 %>% 
+  map(~ mtgonga)
+
+
+
+# ----------------------------------------
+#### OLD MT GONGA STUFF ####
+
 # Importing files, sheet L
 gonga_L <- read_excel("biomass2015.xls", sheet = 1)
 names(gonga_L) # cheching names -> looks ok 
@@ -25,6 +51,8 @@ str(gonga_H) # checking structure -> looks ok
 
 # plot by 
 # ggplot grouped by 
+
+
 
 
 
